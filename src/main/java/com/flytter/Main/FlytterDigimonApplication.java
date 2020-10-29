7fccd5e8549af1fac274a31f213a92403b05eccf
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-10-28 23:29:33 -0300

    Projeto criado

---
+++ src/main/java/com/flytter/Main/FlytterDigimonApplication.java
@@ -0,0 +1,13 @@
+package com.flytter.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlytterDigimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlytterDigimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flytter/Main/FlytterDigimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flytter.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlytterDigimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
